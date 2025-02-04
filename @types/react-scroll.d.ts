1359a839dfe08557a22d6c8910cca5d8161fe1da
kaviska <126580314+kaviska@users.noreply.github.com>
2025-02-04 22:57:03 +0530

    update

---
+++ @types/react-scroll.d.ts
@@ -0,0 +1,7 @@
+declare module "react-scroll" {
+    export const Link: React.ComponentType<any>;
+    export const Element: any;
+    export const scroller: any;
+    export const animateScroll: any;
+  }
+  
